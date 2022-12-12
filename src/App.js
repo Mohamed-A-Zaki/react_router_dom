75f3cd31118a545e190fe20c3fb9c1cefe46a59b
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2022-12-12 15:13:01 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  // console.log(useLoaderData());
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
